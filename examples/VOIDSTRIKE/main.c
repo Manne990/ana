ff3b9121c1fba94a15f986104263b84e2cd5bf1a
Manne990 <jonas.frid@me.com>
2026-07-18 14:03:20 +0200

    Reduce VOIDSTRIKE render cost

--- examples/VOIDSTRIKE/main.c
+++ examples/VOIDSTRIKE/main.c
@@ -1,6 +1,6 @@
 #include "ana.h"
 #include "voidstrike_game.h"
-int main(void) { ANA_Game game = {0}; game.init=voidstrike_init; game.load=voidstrike_load; game.update=voidstrike_update; game.draw=voidstrike_draw; game.shutdown=voidstrike_shutdown; game.width=ANA_DEFAULT_WIDTH; game.height=ANA_DEFAULT_HEIGHT; game.fps=ANA_DEFAULT_FPS; game.colors=ANA_DEFAULT_COLORS; game.screen_mode=ANA_SCREEN_PAL_LORES; game.render_mode=ANA_RENDER_VERTICAL_SCROLL;
+int main(void) { ANA_Game game = {0}; game.init=voidstrike_init; game.load=voidstrike_load; game.update=voidstrike_update; game.draw=voidstrike_draw; game.shutdown=voidstrike_shutdown; game.width=ANA_DEFAULT_WIDTH; game.height=ANA_DEFAULT_HEIGHT; game.fps=ANA_DEFAULT_FPS; game.colors=ANA_DEFAULT_COLORS; game.screen_mode=ANA_SCREEN_PAL_LORES; game.render_mode=ANA_RENDER_DIRTY;
 #ifdef ANA_DEBUG_STATS
 game.debug_stats=1;
 #endif
